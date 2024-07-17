ca59f0d8853315bb7391c0a61e7e7d04aed185a5
Pedro Vieira <80628417+PedroPog@users.noreply.github.com>
2024-07-17 17:29:38 -0300

    NGX

---
+++ src/app/components/glitch/glitch.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'error-glitch',
+  standalone: true,
+  imports: [],
+  templateUrl: './glitch.component.html',
+  styleUrl: './glitch.component.scss'
+})
+export class GlitchComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from "../../components/carousel/carousel.component";
+import { GlitchComponent } from "../../components/glitch/glitch.component";
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [
+    CarouselComponent,
+    GlitchComponent,
+    CarouselModule,
+    ButtonModule,
+    TagModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  responsiveOptions: any[] | undefined;
+  items = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg",
+    "image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"];
+
+  ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+          breakpoint: '1199px',
+          numVisible: 1,
+          numScroll: 1
+      },
+      {
+          breakpoint: '991px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 1,
+          numScroll: 1
+      }
+    ];
+  }
+
 
 }
